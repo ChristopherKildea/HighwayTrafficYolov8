a231cb24d9e0167b13b954547737f29178aa97d6
ChristopherKildea <122947271+ChristopherKildea@users.noreply.github.com>
2023-12-27 12:22:33 -0500

    Now only tracks cars and trucks

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ def graph_data(graph_list):
 
     # Display the plot
     plt.show()
+
 def record_traffic_percentage(times_list, time):
 
     avg_time = sum(times_list) / len(times_list)
@@ -81,6 +82,9 @@ def record_traffic_percentage(times_list, time):
         percent_diff = 0
 
     percent_diff_list.append([time, percent_diff])  # 1 will be replaced whatever the correct time is
+
+
+
 
 
 
@@ -105,68 +109,75 @@ while cap.isOpened():
         cv2.line(frame, (0, tracking_end), (2000, tracking_end), (0, 0, 255), 3)
 
         # Run YOLOv8 tracking on the frame, persisting tracks between frames
-        results = model.track(frame, persist=True, conf=0.1)
-
-        # Get the boxes and track IDs
-        boxes = results[0].boxes.xywh.cpu()
-        vehicle_ids = results[0].boxes.id.int().cpu().tolist()
-
-        # Visualize the results on the frame
-        annotated_frame = results[0].plot()
-        # Display the annotated frame
-        annotated_frame = cv2.resize(annotated_frame, (960,540))
-        cv2.imshow("YOLOv8 Tracking", annotated_frame)
-
-        # Find the current time on the video
-        frame_count += 1
-        vid_time = frame_count / fps  # works
-
-        # Plot the tracks
-        for box, vehicle_id in zip(boxes, vehicle_ids):
-
-            # Dimensions of the bounding box
-            x, y, w, h = box
-
-            # Check if the box has passed the first line
-            if y > tracking_begin:
-
-                # print(f"Passed tracking_begin: {track_id}")
-
-                if y < tracking_end:  # The box is in between the first and seconds lines; add to its frame history
-                    # print(f"Between lines: {track_id}")
-                    vehicle_frames[vehicle_id] += 1
-                elif y > tracking_end and vehicle_frames[vehicle_id] != 0:  # Below ending line and hasn't been recorded
-                    # calculate/record time
-                    # print(f"Passed tracking_end: {track_id}")
-                    # Calculate the time the vehicle took to cross from the beginning to the end line
-                    # based on the number of frames it was between the lines and the fps
-
-                    vehicle_times.append(vehicle_frames[vehicle_id] / fps)
-
-                    # set its value in the list to 0
-                    vehicle_frames[vehicle_id] = 0
-
-        # If we have surpassed our time interval, process
-        # the data for the given time interval
-        if vid_time > total_time_requirement:
-
-            # If there is data to collect
-            if len(vehicle_times) > 0:
-
-                # This works
-
-                # print(f"total_time_requirement: {total_time_requirement}")
+        results = model.track(frame, persist=True, conf=0.1, classes=[2,7])
 
 
-                # Find average
-
-                # Process the data
-                record_traffic_percentage(vehicle_times, total_time_requirement)
-                vehicle_times.clear()
+        if results[0] != None and results[0].boxes.id != None:
 
 
-            # Calculate the next time to collect data
-            total_time_requirement += data_collection_interval
+            # Get the boxes and track IDs
+            boxes = results[0].boxes.xywh.cpu()
+            vehicle_ids = results[0].boxes.id.int().cpu().tolist()
+
+
+
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
+
+            # Display the annotated frame
+            annotated_frame = cv2.resize(annotated_frame, (960,540))
+            cv2.imshow("YOLOv8 Tracking", annotated_frame)
+
+            # Find the current time on the video
+            frame_count += 1
+            vid_time = frame_count / fps  # works
+
+            # Plot the tracks
+            for box, vehicle_id in zip(boxes, vehicle_ids):
+
+                # Dimensions of the bounding box
+                x, y, w, h = box
+
+                # Check if the box has passed the first line
+                if y > tracking_begin:
+
+                    # print(f"Passed tracking_begin: {track_id}")
+
+                    if y < tracking_end:  # The box is in between the first and seconds lines; add to its frame history
+                        # print(f"Between lines: {track_id}")
+                        vehicle_frames[vehicle_id] += 1
+                    elif y > tracking_end and vehicle_frames[vehicle_id] != 0:  # Below ending line and hasn't been recorded
+                        # calculate/record time
+                        # print(f"Passed tracking_end: {track_id}")
+                        # Calculate the time the vehicle took to cross from the beginning to the end line
+                        # based on the number of frames it was between the lines and the fps
+
+                        vehicle_times.append(vehicle_frames[vehicle_id] / fps)
+
+                        # set its value in the list to 0
+                        vehicle_frames[vehicle_id] = 0
+
+            # If we have surpassed our time interval, process
+            # the data for the given time interval
+            if vid_time > total_time_requirement:
+
+                # If there is data to collect
+                if len(vehicle_times) > 0:
+
+                    # This works
+
+                    # print(f"total_time_requirement: {total_time_requirement}")
+
+
+                    # Find average
+
+                    # Process the data
+                    record_traffic_percentage(vehicle_times, total_time_requirement)
+                    vehicle_times.clear()
+
+
+                # Calculate the next time to collect data
+                total_time_requirement += data_collection_interval
 
         # we can loop through the object's bounding boxes and see if they are below a certain point
         # Break the loop if 'q' is pressed
